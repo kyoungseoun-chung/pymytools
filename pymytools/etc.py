f2e1f24b5c464f471f74d480c780f2e59adca0d4
Kyoungseoun Chung <kyoungseoun.chung@gmail.com>
2023-03-26 04:33:57 +0200

    fix: :bug: change assets directory and also fix the asset path

--- pymytools/etc.py
+++ pymytools/etc.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
 """Un-categorized feature of `pymytools`."""
-
-from pathlib import Path
-import simpleaudio as sa
-from pymytools.logger import markup, console, SUPPORTING_COLORS
 import time
+
+import simpleaudio as sa
+from pkg_resources import resource_filename
+
+import pymytools
+from pymytools.logger import console
+from pymytools.logger import markup
+from pymytools.logger import SUPPORTING_COLORS
 
 
 class Me:
     """Memes about me. :D"""
 
-    _path_to_life: Path = Path(
-        Path(__file__).parent.parent, "assets/sounds/life_is.wav"
+    _path_to_life: str = resource_filename(
+        pymytools.__name__, "assets/sounds/life_is.wav"
     )
 
     @property
     def how_is_your_life(self) -> None:
-
-        wave_obj = sa.WaveObject.from_wave_file(self._path_to_life.as_posix())
+        wave_obj = sa.WaveObject.from_wave_file(self._path_to_life)
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        
+
     @property
     def python_is(self) -> None:
-        
         for c in SUPPORTING_COLORS:
-            console.print("\t🔥 " + markup("STATICALLY TYPED", c, "bold")+" 🔥", end="\r")
+            console.print(
+                "\t🔥 " + markup("STATICALLY TYPED", c, "bold") + " 🔥", end="\r"
+            )
             time.sleep(0.5)
         print("")
 
